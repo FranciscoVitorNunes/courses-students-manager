01a2818fa7fa9cb001f5835958079c608c7cfdd7
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-11-30 20:14:03 -0300

    Implementa a classe AlunoRepository com métodos para salvar, buscar, listar, deletar e atualizar alunos no banco de dados.

---
+++ repositories/aluno_repository.py
@@ -0,0 +1,72 @@
+from database import connection 
+from models import Aluno
+
+class AlunoRepository:
+    def __init__(self):
+        self.conn , self.cursor = connection.SQLiteConnection.get_connection()
+    
+    def salvar_aluno(self, aluno: Aluno):
+        sql= """
+            INSERT INTO aluno(matricula, nome, email)
+            VALUES (?, ?, ?)
+        """
+
+        self.cursor.execute(sql, (aluno.matricula, aluno.nome, aluno.email))
+        self.conn.commit()
+        print("Aluno criado com sucesso!")
+
+    def buscar_por_matricula(self, matricula: str) -> Aluno | None:
+        sql = """
+            SELECT * FROM aluno WHERE matricula = ?;
+        """
+
+        self.cursor.execute(sql, (matricula, ))
+        row = self.cursor.fetchone()
+
+        if row is None:
+            return None
+
+        return Aluno(matricula=row['matricula'], nome=row['nome'], email=row['email'])
+    
+    def listar_alunos(self) -> list[Aluno] :
+
+        sql = """
+            SELECT * FROM aluno;
+        """
+
+        self.cursor.execute(sql)
+        all_row = self.cursor.fetchall()
+
+        if all_row is None:
+            return []
+
+        alunos = []
+
+        for row in all_row:
+            aluno = Aluno(matricula=row['matricula'], nome=row['nome'], email=row['email'])
+
+            alunos.append(aluno)
+
+        return alunos
+    
+    def deletar_por_matricula(self, matricula: str):
+        sql = """
+            DELETE FROM aluno WHERE matricula = ?;
+        """
+        
+        self.cursor.execute(sql, (matricula, ))
+        self.conn.commit()
+        print("Deletado com sucesso!")
+    
+        return 
+    
+    def atualizar_aluno(self, aluno: Aluno):
+        sql = """
+            UPDATE aluno
+            SET nome = ?, email = ?
+            WHERE matricula = ?;
+        """
+
+        self.cursor.execute(sql, (aluno.nome, aluno.email, aluno.matricula))
+        self.conn.commit()
+        print("Atualizado com sucesso!")
