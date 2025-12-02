ad0163ebd8b1917212f15e68d1c2134d335c1f5b
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-12-02 16:43:59 -0300

    Repositório da matricula. Funções adicionadas: salvar buscar_por_aluno_e_turma contar_matriculas_por_turma listar_turmas_por_aluno

---
+++ repositories/maticula_repository.py
@@ -0,0 +1,52 @@
+from database.connection import  SQLiteConnection
+from models.matricula import Matricula
+
+class MatriculaRepository:
+    def __init__(self):
+        self.conn , self.cursor = SQLiteConnection.get_connection()
+    
+    def salvar(self, matricula: Matricula):
+        sql= """
+            INSERT INTO matricula(aluno_matricula, turma_id) VALUES (?, ?)
+        """
+
+        self.cursor.execute(sql, (matricula.aluno.matricula, matricula.turma.id))
+        self.conn.commit()
+        print("Matrícula realizada com sucesso!")
+    
+    def buscar_por_aluno_e_turma(self, aluno_matricula: str, turma_id: str) -> bool:
+        """Retorna True se matricula existir"""
+        sql= """
+            SELECT * FROM matricula WHERE aluno_matricula = ? AND turma_id = ?
+        """
+
+        self.cursor.execute(sql, (aluno_matricula, turma_id))
+        row = self.cursor.fetchone()
+
+        if row is None:
+            return None
+
+        return True
+    
+    def contar_matriculas_por_turma(self, turma_id: int) -> int:
+
+        sql = """
+            SELECT COUNT(*) FROM matricula WHERE turma_id = ?
+        """
+        
+        self.cursor.execute(sql, (turma_id,))
+        
+        contagem = self.cursor.fetchone()[0]
+        
+        return contagem
+
+    def listar_turmas_por_aluno(self, aluno_matricula: str) -> list[str]:
+        sql = """
+            SELECT turma_id FROM matricula WHERE aluno_matricula = ?;
+        """
+
+        self.cursor.execute(sql, (aluno_matricula, ))
+        all_row = self.cursor.fetchall()
+
+        turmasIDs = [row['turma_id'] for row in all_row]
+        return turmasIDs
