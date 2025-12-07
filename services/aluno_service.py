63e6b96d4fae5af730ae05b560f63a9c056d5315
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-12-07 10:34:00 -0300

    feat(service): Implementa camada service do aluno

---
+++ services/aluno_service.py
@@ -0,0 +1,31 @@
+from repositories.aluno_repository import AlunoRepository
+
+class AlunoService:
+    def __init__(self, aluno):
+        self.aluno = aluno
+        
+    def criar_aluno_service(self):
+        # Verificar se matricula já existe
+        try:
+            AlunoRepository.salvar(self.aluno)
+        except:
+            return False
+        
+    def deletar_aluno_service(self, matricula):
+        try:
+            AlunoRepository.deletar(matricula)
+        except:
+            return False
+        
+    def atualizar_aluno_service(self, matricula, aluno):
+        try:
+            AlunoRepository.atualizar(aluno)
+        except:
+            return False
+
+    def buscar_aluno_service(self, matricula):
+        try:
+            AlunoRepository.buscar_por_matricula(matricula)
+        except:
+            return False
+
