c120f2dc781df4a0253a7d0caa046672d49fc9ba
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-12-09 09:24:55 -0300

    feat(main): Cria servidor e implementa aluno_router

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from routers import aluno_router
+
+app = FastAPI(title="Gerenciador de Cursos e Alunos")
+
+@app.get("/")
+def teste():
+    return "Gerenciador de Cursos e Alunos"
+
+app.include_router(aluno_router.router)
+
