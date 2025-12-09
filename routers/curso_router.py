289c47bddf15865db829772cc2a3e2f4e360ecb3
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-12-09 14:58:00 -0300

    feat(prerequistos): Cria rotas de criar e buscar prerequisitos

--- routers/curso_router.py
+++ routers/curso_router.py
@@ -42,3 +42,17 @@ def atualizar(codigo: str, curso: UpdateCursoSchema):
         )
 
     return {"message": "Curso atualizado parcialmente"}
+
+@router.post("/prerequisitos")
+def create_prerequisito(curso_codigo, curso_codigo_prereq):
+    try:
+        service.create_prerequisitos(curso_codigo, curso_codigo_prereq)
+    except Exception:
+        raise HTTPException(status_code=400, detail="Erro ao adicionar prerequisito")
+
+@router.get("/prerequisitos/{codigo}")
+def buscar_prereqq_por_codigo(codigo: str):
+    curso = service.get_prerequisitos(codigo)
+    if not curso:
+        raise HTTPException(status_code=404, detail="Prerequisitos não encontrado")
+    return curso
